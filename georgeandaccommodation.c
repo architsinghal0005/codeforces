c20878b1c6263206fe8cb0e26913b5887f523594
architsinghal0005 <architsinghal941@gmail.com>
2025-10-29 00:48:50 +0530

    467A (george and accomodation)

---
+++ georgeandaccommodation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+   int n,i,p,q,count=0;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+    scanf("%d %d",&p,&q);
+    if(q-p>=2)
+    {
+        count++;
+    }
+   }
+   printf("%d",count);
+    
+    return 0;
+}
