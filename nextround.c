584bc6c08162464a00af1863747587047a99c7e6
architsinghal0005 <architsinghal941@gmail.com>
2025-11-03 20:48:50 +0530

    158A (next round)

---
+++ nextround.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, k, i;
+    scanf("%d %d", &n, &k);
+    int a[n], count = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] >= a[k-1] && a[i] > 0)
+        {
+            count++;
+        }
+    }
+    printf("%d", count);
+
+    return 0;
+}
