b4a43fe7512be9240ba5b6148ae3e5b6216a9092
architsinghal0005 <architsinghal941@gmail.com>
2025-11-09 21:31:02 +0530

    705A (hulk)

---
+++ hulk.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        if (i % 2 != 0)
+        {
+            printf("I hate");
+        }
+        else
+        {
+            printf("I love");
+        }
+        if (i == n)
+        {
+            printf(" it");
+        }
+        else
+        {
+            printf(" that ");
+        }
+    }
+
+    return 0;
+}
