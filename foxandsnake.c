31a19abdc21946f634c5fa48016063ab2a02c39f
architsinghal0005 <architsinghal941@gmail.com>
2025-11-11 21:49:09 +0530

    510A (fox and snake)

---
+++ foxandsnake.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, m, i, j, k;
+    scanf("%d %d", &n, &m);
+    for (i = 1; i <= n; i++)
+    {
+        if (i % 2 != 0)
+        {
+            for (j = 1; j <= m; j++)
+            {
+                printf("#");
+            }
+        }
+        else if (i % 4 == 0)
+        {
+            printf("#");
+            for (j = 2; j <= m; j++)
+            {
+                printf(".");
+            }
+        }
+        else
+        {
+            for (j = 1; j < m; j++)
+            {
+                printf(".");
+            }
+            printf("#");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
