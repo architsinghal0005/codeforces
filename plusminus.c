1bb257c33058b054f1716badd6b94909041e7244
architsinghal0005 <architsinghal941@gmail.com>
2025-11-22 19:50:37 +0530

    1807A (plus or minus)

---
+++ plusminus.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int t,a,b,c;
+    scanf("%d",&t);
+    while (t--)
+    {
+       scanf("%d %d %d",&a,&b,&c);
+       if(a+b==c)
+       {
+        printf("+\n");
+       }
+       if(a-b==c)
+       {
+        printf("-\n");
+       }
+    }
+    
+    
+    return 0;
+}
