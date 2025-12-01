28cf858ae0771ba4ebf5d3c784ca41b93012d7d4
architsinghal0005 <architsinghal941@gmail.com>
2025-12-01 21:44:04 +0530

    1850A (to my critics)

---
+++ tomycritics.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for(int i=0;i<t;i++) {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+
+        if (a + b >= 10 || b + c >= 10 || c + a >= 10)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
