d3c86266a349b6260517f02831f8469828b63090
architsinghal0005 <architsinghal941@gmail.com>
2025-12-08 18:02:12 +0530

    1560A (dislike of trees)

---
+++ dislikeoftrees.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for(int i=0;i<t;i++) {
+        int k;
+        scanf("%d", &k);
+
+        int count = 0;
+        int num = 1;
+
+        while (1) {
+            if (num % 3 != 0 && num % 10 != 3) {
+                count++;
+                if (count == k) {
+                    printf("%d\n", num);
+                    break;
+                }
+            }
+            num++;
+        }
+    }
+
+    return 0;
+}
