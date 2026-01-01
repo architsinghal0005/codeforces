9fced761b88f8f438d196174d39a146e34b1fcf2
architsinghal0005 <architsinghal941@gmail.com>
2026-01-01 23:41:16 +0530

    1866A (ambitious kid)

---
+++ ambitiouskid.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        if (a[i] < 0)
+        {
+            a[i] = -a[i];
+        }
+        else
+        {
+            a[i] = a[i];
+        }
+    }
+    int temp = a[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] <= temp)
+        {
+            temp = a[i];
+        }
+    }
+    printf("%d", temp);
+
+    return 0;
+}
